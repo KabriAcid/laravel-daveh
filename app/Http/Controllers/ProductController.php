5539530e55fec8ce830a6a357f012de1e2d9dd95
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-24 22:59:05 +0100

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,6 +14,10 @@ class ProductController extends Controller
     }
     public function store(Request $request){
         $product = new Product();
-        
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->size = $request->size;
+
+        $product->save();
     }
 }
