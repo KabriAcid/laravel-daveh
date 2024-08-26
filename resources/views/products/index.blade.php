8cb1b916f6f830ffebd38619656982fffeab4aec
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-26 11:57:01 +0100

    "Fixed typo in route name and removed semicolon after endforeach"

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
     <h1>Products</h1>
 
-    <a href="{{ route('products.create) }}">New Product</a>
+    <a href="{{ route('products.create') }}">New Product</a>
 
     @foreach ($products as $product)
     <h2>{{ $product->name }}</h2>
     <p>{{ $product->description }}</p>
     <p>{{ $product->size }}</p>
-    @endforeach;
+    @endforeach
 </x-layout>
