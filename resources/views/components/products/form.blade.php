fb37065e0318076e9dcfb634594d43b1efeb20aa
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-29 23:53:18 +0100

    "Added null coalescing operator (?? '') to input values in product form"

--- resources/views/components/products/form.blade.php
+++ resources/views/components/products/form.blade.php
@@ -2,17 +2,17 @@
 <!-- Name -->
 <div>
     <label for="">Name</label>
-    <input type="text" value="{{ old('name', $product->name)}}" name="name">
+    <input type="text" value="{{ old('name', $product->name ?? '')}}" name="name">
 </div>
 <!-- Description -->
 <div>
     <label for="">Description</label>
-    <textarea name="description">{{ old('description', $product->description)}}</textarea>
+    <textarea name="description">{{ old('description', $product->description ?? '')}}</textarea>
 </div>
 <!-- Size -->
 <div>
     <label for="">Size</label>
-    <input type="number" value="{{ old('size', $product->size)}}" name="size">
+    <input type="number" value="{{ old('size', $product->size ?? '')}}" name="size">
 </div>
 <!-- Button -->
 <button>Save</button>
