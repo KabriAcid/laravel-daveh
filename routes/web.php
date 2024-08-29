bab9453da2aa083868a414c213dafcd35fe2b9cb
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-30 00:48:46 +0100

    "Added Laravel Debugbar package, updated composer files, and removed controller references from route definitions in routes/web.php"

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,18 @@ Route::controller(ProductController::class)->group(function (){
 
     Route::view('/', 'home');
     
-    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products')->name('products.index');
     
-    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::get('/products/create')->name('products.create');
     
-    Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::post('/products/store')->name('products.store');
     
-    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/products/{product}')->name('products.show');
     
-    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::get('/products/{product}/edit')->name('products.edit');
     
-    Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::patch('/products/{product}')->name('products.update');
     
-    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::delete('/products/{product}')->name('products.destroy');
     
 });
