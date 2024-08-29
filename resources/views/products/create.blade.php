428ed3bbaa3696d8cb908ae437fcb1f335574ee4
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-29 23:44:50 +0100

    "Added error display and form to create and edit product views"

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,8 +1,9 @@
 <x-layout>
     <h1>New Product</h1>
-
+    {{-- Display errors --}}
     <x-errors/>
 
+    {{-- Form --}}
     <form action="{{ route('products.store') }}" method="post">
       <x-products.form/>
     </form>
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,4 +1,10 @@
 <x-layout>
     <h1>Edit Product</h1>
+    {{-- Display errors --}}
+    <x-errors/>
     
+    {{-- Form --}}
+    <form action="{{ route('products.store') }}" method="post">
+      <x-products.form/>
+    </form>    
 </x-layout>
