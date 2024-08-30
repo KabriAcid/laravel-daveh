096f15f3f5dd3f8f8e153d0b0ac16263537c9231
Abdullahi Kabri <kabriacid01@gmail.com>
2024-08-30 01:07:08 +0100

    "Removed laravel-debugbar from composer dependencies, updated composer.lock, modified home.blade.php to use x-layout, and refactored routes in web.php"

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-
+use App\Models\Product as P;
+Route::view('/', 'home');
 
 Route::controller(ProductController::class)
     ->prefix('products')
     ->name('products.')
-    ->group(function (){
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
 
-    Route::view('/', 'home');
-    
-    Route::get('/products')->name('index');
-    
-    Route::get('/create')->name('create');
-    
-    Route::post('/store')->name('store');
-    
-    Route::get('/{product}')->name('show');
-    
-    Route::get('/{product}/edit')->name('edit');
-    
-    Route::patch('/{product}')->name('update');
-    
-    Route::delete('/{product}')->name('destroy');
-    
-});
+        // Define a route model binding for the {product} parameter
+        Route::bind('product', function ($value) {
+            return P::where('id', $value)->firstOrFail();
+        });
+
+        Route::get('/{product}', 'show')->name('show');
+        Route::get('/{product}/edit', 'edit')->name('edit');
+        Route::patch('/{product}', 'update')->name('update');
+        Route::delete('/{product}', 'destroy')->name('destroy');
+    });
